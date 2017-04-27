1169a33fa32d61ded932824f0bd9bf948ad3dbb6
Usuario Local <Usuario Local>
2017-04-27 15:59:59 +0200

    Touch support

--- game.js
+++ game.js
@@ -73,8 +73,10 @@ window.addEventListener("load",function() {
 		step: function(dt) {
 			//console.log(this.p.y);
 			if(this.p.y > 610) { //falls
-				this.p.x = 150;
-				this.p.y = 380;
+				//this.p.x = 150;
+				//this.p.y = 380;	
+				this.destroy();
+				Q.stageScene("endGame",1, { label: "You Lose" });
 			}
 		},
 
@@ -219,10 +221,16 @@ window.addEventListener("load",function() {
 	//Q.stageScene("endGame",1, { label: "You Win" });
 	Q.scene('endGame',function(stage) {
 		var container = stage.insert(new Q.UI.Container({ x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)" }));
-		var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC", label: "Play Again" }))
+		var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC", label: "Play Again" }));
 		var label = container.insert(new Q.UI.Text({x:10, y: -10 - button.p.h, label: stage.options.label }));
 		
 		button.on("click",function() {
+			Q.clearStages();
+			Q.stageScene('level1');
+			//Q.stageScene('titleScreen');
+		});
+
+		button.on("push",function() {
 			Q.clearStages();
 			Q.stageScene('level1');
 			//Q.stageScene('titleScreen');
